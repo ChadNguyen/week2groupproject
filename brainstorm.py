030b069ace0f44255f123a838748f352f6ef5cee
Chad Nguyen <chadn458@gmail.com>
2023-02-03 16:43:25 -0500

    chad

---
+++ brainstorm.py
@@ -0,0 +1,32 @@
+
+
+
+# spaces increase and decrease: not done
+# cars entering exiting ticket count increase: done
+# pay diallogue: not done
+# parkingspace list: not done
+# tickets: half done(need total count at end of the day)
+
+
+
+# Your parking gargage class should have the following methods:
+# takeTicket
+# This should decrease the amount of tickets available by 1 ##################### DONE
+# This should decrease the amount of parkingSpaces available by 1 ##################### DONE
+
+
+
+#  payForParking
+#  Display an input that waits for an amount from the user and store it in a variable
+#  If the payment variable is not empty then (meaning the ticket has been paid) ->  display a message to the user that their ticket has been paid and they have 15mins to leave
+#  This should update the "currentTicket" dictionary key "paid" to True
+
+
+
+
+#  leaveGarage
+#  If the ticket has been paid, display a message of "Thank You, have a nice day"
+#  If the ticket has not been paid, display an input prompt for payment
+#  Once paid, display message "Thank you, have a nice day!"
+#  Update parkingSpaces list to increase by 1 (meaning add to the parkingSpaces list)
+#  Update tickets list to increase by 1 (meaning add to the tickets list)
