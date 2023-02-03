b7740b7305feb03e7f7f7f639d2c1ba1506fe46e
Chad Nguyen <chadn458@gmail.com>
2023-02-03 16:34:10 -0500

    touched up both sides code to merge

--- parkingGarage.py
+++ parkingGarage.py
@@ -1,11 +1,9 @@
-# Start Your Code here
-
-
-====
 # You will need a few attributes as well:
 # - tickets -> list
 # - parkingSpaces -> list
 # - currentTicket -> dictionary
+
+# Start Your Code here
 
 class Garagelot:
     def __init__(self, vehicle, pay=10, dailytotal, availablespaces=100, tickets=0,):
@@ -19,10 +17,15 @@ class Garagelot:
     def vehicle_entering(self, parked=1, sold_tickets=1):
         self.availablespaces += parked
         self.tickets += sold_tickets 
+        if self.availablespaces >= 0:
+            print('Welcome to the parking garage! Enjoy your time here!')
+        else:
+            print('Sorry, there are no more available parking spaces.')
 
     
     def vehicle_exiting(self, exiting=1):
         self.availablespaces -= exiting
+        print('Goodbye, thank you for coming! Have a wonderful day!')
         
 
     def driver():
@@ -31,21 +34,7 @@ class Garagelot:
 
 
         
-class ParkingGarage:
-    def __init__(self, space_qty=100):
-        self.space_qty = space_qty
-        self.open_spaces = space_qty
-        
-    def enter(self, customer_qty):
-        self.open_spaces -= customer_qty
-        if self.open_spaces >= 0:
-            print('Welcome to the parking garage! Enjoy your time here!')
-        else:
-            print('Sorry, there are no more available parking spaces.')
-    
-    def leave(self, customer_qty):
-        self.open_spaces += customer_qty
-        print('Goodbye, thank you for coming! Have a wonderful day!')
+
 
 
 
