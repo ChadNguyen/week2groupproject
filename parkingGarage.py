b2c2a487afe8ee265acb6bdc0938b9f8ec63392e
Zion Martin <zima67053@gmail.com>
2023-02-03 14:52:59 -0600

    Zee_changes

--- parkingGarage.py
+++ parkingGarage.py
@@ -1,1 +1,16 @@
 # Start Your Code here
+class ParkingGarage:
+    def __init__(self, space_qty=100):
+        self.space_qty = space_qty
+        self.open_spaces = space_qty
+        
+    def enter(self, customer_qty):
+        self.open_spaces -= customer_qty
+        if self.open_spaces >= 0:
+            print('Welcome to the parking garage! Enjoy your time here!')
+        else:
+            print('Sorry, there are no more available parking spaces.')
+    
+    def leave(self, customer_qty):
+        self.open_spaces += customer_qty
+        print('Goodbye, thank you for coming! Have a wonderful day!')
